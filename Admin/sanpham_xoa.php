e7b3d78fa1001d3c27a26987607533aae4be06b5
dhthanh05apr10th <dhthanh.05april10th@gmail.com>
2025-11-14 21:03:37 +0700

    .

--- Admin/sanpham_xoa.php
+++ Admin/sanpham_xoa.php
@@ -9,21 +9,12 @@
 
     if (isset($_GET['id'])) 
     {
+
         $product_id_to_delete = $_GET['id'];
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "qltp";
+        require_once 'cauhinh.php';
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) 
-        {
-            die("Kết nối thất bại: " . $conn->connect_error);
-        }
-
-        $conn->set_charset("utf8");
         $sql = "DELETE FROM san_pham WHERE id_san_pham = ?";
-        $stmt = $conn->prepare($sql);
+        $stmt = $connect->prepare($sql);
 
         if ($stmt) 
         {
@@ -34,7 +25,7 @@
             $stmt->close();
         }
         
-        $conn->close();
+        $connect->close();
 
         header("Location: danhsachsanpham.php");
         exit;
